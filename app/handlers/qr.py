f43c68a7169890a824ed4f18c47d71f8931e51ec
MHanushchak <misha.ganushchak@gmail.com>
2026-08-08 14:13:23 +0300

    get qr

--- app/handlers/qr.py
+++ app/handlers/qr.py
@@ -1,18 +1,25 @@
 from aiogram import Router
-from aiogram.types import Message
-from aiogram.filters import CommandStart, CommandObject
+from aiogram.types import BufferedInputFile, Message
+from aiogram.filters import CommandStart, CommandObject, Command
+from app.database.database import session_factory
+from app.services.qr_service import create_qr_pass
 
-router = Router()
+qr = Router()
 
 
-@router.message(CommandStart())
-async def start_handler(message: Message):
-    await message.answer(
-        f"Привіт, {message.from_user.first_name}! 👋"
+@qr.message(Command("qr"))
+async def generate_qr_handler(message: Message):
+    async with session_factory() as session:
+        pass_id, qr_file = await create_qr_pass(session, message.from_user.id)
+
+    photo = BufferedInputFile(
+        qr_file.getvalue(),
+        filename=f"{pass_id}.png"
     )
+    await message.answer_photo(photo=photo, caption="Ваш QR-пропуск створено")
 
 
-@router.message(CommandStart(deep_link=True))
+@qr.message(CommandStart(deep_link=True))
 async def scan_qr_handler(message: Message, command: CommandObject):
     scanned_uuid = command.args
     await message.answer(f"UUID: {scanned_uuid}")
--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 import asyncio
-import os
 
-from aiogram import Bot, Dispatcher
-from dotenv import load_dotenv
-from app.handlers.qr import router
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
-    dp.include_router(router)
-    await dp.start_polling(bot)
+from app.bot import start_bot
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(start_bot())
