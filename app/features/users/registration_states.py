dbb9f354753baa124273fa69d53c0c09aa288181
studentssmm-wq <students.smm@lpnu.ua>
2026-08-28 09:15:34 +0200

    Update registration_states.py

--- app/features/users/registration_states.py
+++ app/features/users/registration_states.py
@@ -2,6 +2,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class Registration(StatesGroup):
+    phone_number = State()
     full_name = State()
     institute = State()
     non_student_role = State()
